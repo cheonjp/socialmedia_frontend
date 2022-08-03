1b54530da014e3ecff5711044fb138b2d8b6c597
cheonjp <65703767+cheonjp@users.noreply.github.com>
2022-08-03 01:00:28 -0600

    designing for register

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Register from "./pages/register/Register";
+
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Register />
     </div>
   );
 }
